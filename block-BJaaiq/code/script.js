20d5e6d0b2533aedf2249aaeb0a0fc6dfc5010e0
Sumit Chahar <chahar.sumit888@gmail.com>
2021-11-05 16:14:51 +0530

    Complete infinite scroll

--- block-BJaaiq/code/script.js
+++ block-BJaaiq/code/script.js
@@ -1,3 +1,4 @@
+/*
 function scrollMe(){
   while(true){
     let bottom = document.documentElement.getBoundingClientRect().bottom;
@@ -26,4 +27,38 @@ window.addEventListener('scroll', scrollMe);
 scrollMe();
 arrow.addEventListener('click', function(){
   window.scrollTo(pageYOffset, 0);
+})
+*/
+
+let noOfQuotesToAdd = 3;
+let root = document.querySelector('ul');
+let index = 0;
+
+function generateQuotes(){
+  for(let i = 0 ; i < noOfQuotesToAdd; i++){
+    let li = document.createElement('li');
+    let blockQuote = document.createElement('blockqoute');
+    blockQuote.innerText = quotes[index].quoteText;
+    blockQuote.classList.add('blockquote');
+    let cite = document.createElement('cite');
+    cite.innerText = quotes[index].quoteAuthor;
+    cite.classList.add('cite');
+    li.append(blockQuote, cite);
+    root.append(li);
+    index++;
+  }
+}
+
+document.addEventListener('scroll', () => {
+  let top = document.documentElement.scrollTop;
+  let height = document.documentElement.clientHeight;
+  let scHeight = document.documentElement.scrollHeight;
+  if(top + height >= scHeight && index < quotes.length){
+    generateQuotes();
+  }
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  alert("DOM content is loaded");
+  generateQuotes();
 })
